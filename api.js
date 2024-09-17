a06da77a05ae9c00193026c594cfe9380729efa5
sandromn <sandromn1973@gmail.com>
2024-09-17 15:40:15 -0300

    alterar disorder

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const port = 3000
 app.use(express.json())
 const cdoctor = require ('./controllers/doctor')
 const cadmin = require ('./controllers/admin')
+const cdisorder = require ('./controllers/disorder')
 
 
 app.post("/doctor",(req, res) => {
@@ -93,6 +94,42 @@ app.delete("/admin/:id", (req, res) => {
     }
 })
 
+app.post("/disorder", (req, res)=>{
+    const {name, cid} = req.body
+
+    if(!name || !cid){
+        res.status(400).json({message: "Os campos name e cid são obrigatório"})
+    }
+
+    let odisorder = cdisorder.create_disorder(name, cid)
+    res.status(200).json({message: "Sucesso", disorder: cdisorder.create_disorder})
+
+})
+
+app.get("/disorder", (req, res) => {
+    return res.status(200).json({message: 'sucesso', disorder: cdisorder.create_disorder()
+    })
+})
+
+app.put("/disorder/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+
+    const {name, cid} = req.body
+
+    let retorno = cdisorder.update_disorder(id, name, cid)
+    return res.status(retorno.status).json(retorno.msg)   
+})
+
+app.delete("/disorder/:id", (req, res)=>{
+     const id = parseInt(req.params.id)
+    //não precisa de body
+
+    if(cdisorder.delete_disorder(id)){
+        return res.status(200).json("foi de base")
+    }else{
+        return res.status(404).json("não encontrado")
+    }
+})
 
 
 app.listen(port, () => {
---
+++ controllers/disorder.js
@@ -0,0 +1,58 @@
+
+
+var  disorders= []
+
+//** vai para o controlador */
+function create_disorder(name, cid){
+    let id = 0
+    if(disorders.length > 0){
+        id = disorders[disorders.length-1].id+1
+    }
+
+    const disorder ={
+        "id": id,
+        "name": name,
+        "cid":cid
+    }
+    disorders.push(disorder)
+    return disorder
+}
+
+function read_disorder(){
+    
+    return disorders
+
+}
+
+function update_disorder(id, name, cid){
+    let idx = disorders.findIndex (disorder=> disorder.id ===id)
+
+    if(idx == -1){
+        return { status: 404, msg: "Não encontrado"}
+    }
+
+    if(name) disorders[idx].name = name
+    if (cid) disorders[idx].cid = cid
+
+    return {status: 200, msg:   disorders[idx]}
+}
+
+function delete_disorder(id){
+    let  idx = disorders.findIndex(disorder => disorder.id ===id)
+    if(idx == -1){
+        return false
+    }
+    disorders.splice(id, 1)
+    return true
+}
+   
+module.exports = {
+
+    create_disorder,
+    read_disorder,
+    update_disorder,
+    delete_disorder
+}
+    
+    
+
