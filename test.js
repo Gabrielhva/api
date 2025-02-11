ce0758fd92a01e3fc043538af3ed508c8c88e4d2
Gabrielhva <148148776+Gabrielhva@users.noreply.github.com>
2025-02-11 14:16:55 -0300

    merge changes

--- test.js
+++ test.js
@@ -3,7 +3,6 @@
 const request = require('supertest');
 const app = require('./api'); // Importa a aplicação
 
-<<<<<<< HEAD
 describe('Testes CRUD para API de Usuários', () => {
     let userId;
 
@@ -75,13 +74,11 @@ describe('Testes CRUD para API de Usuários', () => {
             .delete(`/doctor/del/${userId}`)
 
         expect(res.status).toBe(201);
-=======
 describe('Testando a API', () => {
     it('Deve retornar um JSON com status 200', async () => {
         const response = await request(app).get(`/admin/read`);
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty('admin');
->>>>>>> 0b8091a409009916ab6b49f779465ad602a521de
     });
 
     let adminId
@@ -145,5 +142,9 @@ describe('Testando a API', () => {
         expect(response.body).toHaveProperty('admin');
     });
 });
+
+    });
+});
+
 
 //npm test
